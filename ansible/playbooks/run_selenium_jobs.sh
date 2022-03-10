add3affd260f494f1191e8468808fc4e6758d99a
walter-cw <walter@clarusway.com>
2022-03-10 17:53:12 +0000

    ad

---
+++ ansible/playbooks/run_selenium_jobs.sh
@@ -0,0 +1,3 @@
+PATH="$PATH:/usr/local/bin"
+ansible-playbook -vvv --connection=local --inventory 127.0.0.1, --extra-vars "workspace=${WORKSPACE} master_public_ip=${GRAND_MASTER_PUBLIC_IP}" ./ansible/playbooks/pb_run_selenium_jobs.yaml
+
